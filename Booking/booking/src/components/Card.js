386cee023c82a75de38b744ca547a729a56160a3
DannyDoneva96 <danielasinmas1@gmail.com>
2023-03-01 12:20:49 +0200

    get books from google api
    found this on stackoverflow
       let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
                        let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
                        if(thumbnail!= undefined && amount !=undefined)

--- Booking/booking/src/components/Card.js
+++ Booking/booking/src/components/Card.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React ,{useState}from 'react'
+import Modal from './Modal'
 
 const Card = ({book}) => {
-  return (
-    <div className="card">
-      <img src='this will be url' alt="" />
-      <div className="bottom">
-        <h3 className="title">jjjjj</h3>
-        <p className="amount">kkkk</p>
-      </div>
-      
-    </div>
-  )
+  const [show,setShow]=useState(false);
+    const [bookItem,setItem]=useState();
+    console.log(book)
+    return (
+        <>
+            {
+                book.map((item) => {
+                    let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                    let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+                    if(thumbnail!= undefined && amount !=undefined)
+                    {
+                        return (
+                            <>
+                            <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
+                                <img src={thumbnail} alt="" />
+                                <div className="bottom">
+                                    <h3 className="title">{item.volumeInfo.title}</h3>
+                                    <p className="amount">&#8377;{amount}</p>
+                                </div>
+                            </div>
+                              <Modal show={show} item={bookItem} onClose={()=>setShow(false)}/>
+                            </>
+                        )
+                    }
+                    
+                })
+            }
+
+        </>
+    )
 }
 
 export default Card
