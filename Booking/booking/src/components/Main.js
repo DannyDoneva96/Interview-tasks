4199408b936196b5b0d9d8881c527ba6f2cc79da
DannyDoneva96 <danielasinmas1@gmail.com>
2023-03-01 19:34:55 +0200

    finished

--- Booking/booking/src/components/Main.js
+++ Booking/booking/src/components/Main.js
@@ -6,12 +6,21 @@ const Header = () => {
   const [search,setSearch]=useState("");
   const [bookData,setData]=useState([]);
   const searchBook=()=>{
+    
+    
+        axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBNqg_-FVIMVUDOA9TpfA5JN_uFkMfJZ58'+'&maxResults=40')
+        .then(res=>setData(res.data.items))
+        .catch(err=>console.log(err)) 
+    }
+
+
+  // const searchBook=()=>{
       
-          axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBNqg_-FVIMVUDOA9TpfA5JN_uFkMfJZ58'+'&maxResults=40')
-          .then(res=>setData(res.data.items))
-          .catch(err=>console.log(err))
-      
-  }
+  //         axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBNqg_-FVIMVUDOA9TpfA5JN_uFkMfJZ58'+'&maxResults=40')
+  //         .then(res=>setData(res.data.items))
+  //         .catch(err=>console.log(err))
+  //        
+  // }
   return (<>
     <div className="header">
       <div className="headline">
@@ -24,20 +33,22 @@ const Header = () => {
       <div className="search">
         <h2 className="search-h2">Find Your Book</h2>
         <div className="search-field">
-          <label htmlFor="search-input"><i class="fa-solid fa-book-open-cover"></i></label>
+          <label htmlFor="search-input"><i className="fa-solid fa-book-open-cover"></i></label>
           <input type="text" name='search-input' className="search-input" placeholder="Enter your Book" 
            value={search} onChange={e=>setSearch(e.target.value)}
-          />
+           />
+        
            
-          <button onClick={searchBook}><i class="fa-solid fa-magnifying-glass"></i></button>
+          <button onClick={()=>searchBook()} ><i className="fa-solid fa-magnifying-glass"></i></button>
         </div>
-        <img src={require('../assets/istockphoto-1140770270-612x612-removebg-preview.png')} alt="" />
+        <img src={require('../assets/Book.jpg')} alt="" />
+          
       </div>
     </div>
     <div className="container">
-      {
+      
       <Card book={bookData}/>
-    }</div>
+    </div>
   </>
   )
 }
